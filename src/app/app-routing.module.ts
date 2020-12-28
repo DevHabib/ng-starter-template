3415a9bd776c1d70625be44593c14a7efae767a1
Habib Rahman <contact@devhabib.com>
2020-12-29 00:22:56 +0600

    Angular starter template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/', pathMatch: 'full' },
+  {
+    path: 'dashboard',
+    loadChildren: () => import('../app/modules/dashboard/dashboard.module').then(m => m.DashboardModule)
+  },
+  {
+    path: '',
+    loadChildren: () => import('../app/modules/main-view/main-view.module').then(m => m.MainViewModule)
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
